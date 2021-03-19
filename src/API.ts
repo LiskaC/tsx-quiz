940af5f4f97e2d595e57ba984b86da37c8e03b0a
Yelizaveta Crofts <yscrofts@hotmail.com>
2021-03-19 16:44:47 +0100

    fixed an idiotic typo which was calling to the wrong API url

--- src/API.ts
+++ src/API.ts
@@ -5,7 +5,7 @@ export enum Difficulty {
 }
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
-    const endpoint = `https://opentbd.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
     console.log(data);
 } 
