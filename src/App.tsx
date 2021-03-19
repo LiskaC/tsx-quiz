990228709ce1020dbc589746ed2cdbd9c0f07ac2
Yelizaveta Crofts <yscrofts@hotmail.com>
2021-03-19 17:51:21 +0100

    added conditions for rendering elements

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+  console.log(questions)
 
   const startTrivia = async() => {
     setLoading(true);
@@ -53,22 +53,30 @@ const App = () => {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading Questions...</p>
-      {/* <QuestionCard
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading && <p>Loading Questions...</p>}  {/* same as { loading ? <p>Loading Questions...</p> : null } - called 'short circuit' */}
+      { !loading && !gameOver && (
+        <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
-        answers={questions[number].answer}
+        answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-       /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+       />
+       )}
+      {!gameOver && !loading 
+        && userAnswers.length === number + 1 
+        && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
