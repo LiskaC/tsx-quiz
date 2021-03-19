ae297bfeb68bdd760d24751cb877d7d520a68751
Yelizaveta Crofts <yscrofts@hotmail.com>
2021-03-19 17:18:13 +0100

    wrote startTrivia function to initialise game conditions

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from "./API";
 
-//Components
+// Components
 import QuestionCard from './components/QuestionCard';
+// Types
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  const startTrivia = async() => {
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
+  const startTrivia = async() => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    )
+    .catch(err => console.log("App.tsx, startTrivia, await fetchQuizQuestions error: ", err));
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -33,14 +58,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answer}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-       />
+       /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
